707adf474409d858816e7345aadfbf09a1cdc0ee
kshitij <kshitij_987@protonmail.com>
2023-03-15 18:59:30 +0530

    added some fonts

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="preconnect" href="https://stijndv.com" />
+      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+    </head>
       <body>{children}</body>
     </html>
   )
